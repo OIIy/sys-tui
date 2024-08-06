71595182845fc65a7050e00151d7f957148602e8
Olly McGilloway <ollymcgilloway@gmail.com>
2024-08-06 23:11:10 +0100

    working on layout

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::io;
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Alignment, Rect},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Style, Stylize},
     symbols::border,
     text::{Line, Span, Text},
     widgets::{
         block::{Position, Title},
-        Block, Paragraph, Widget, Wrap,
+        Block, Borders, Paragraph, Widget, Wrap,
     },
     Frame,
 };
@@ -36,7 +36,26 @@ impl App<'_> {
     }
 
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
+        // frame.render_widget(self, frame.size());
+        let mut cols: Vec<Constraint> = vec![];
+
+        for cpu in self.system.cpus() {
+            let col_size: usize = 100 / self.system.cpus().len();
+            cols.push(Constraint::Percentage(col_size.try_into().unwrap()));
+        }
+
+        // Horizontal for columns, Vertical for rows
+        let outer_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(cols)
+            .split(frame.size());
+
+        for (index, cpu) in self.system.cpus().iter().enumerate() {
+            frame.render_widget(
+                Paragraph::new(cpu.name()).block(Block::new().borders(Borders::ALL)),
+                outer_layout[index],
+            );
+        }
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -64,47 +83,6 @@ impl App<'_> {
 
 fn to_gigabytes(bytes: u64) -> f32 {
     ((bytes as f32 / 1024.0) / 1024.0) / 1024.0
-}
-
-impl Widget for &App<'_> {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(self.name.clone().bold());
-        let text = vec![
-            Line::from(vec![
-                Span::raw("Total Memory (GB): "),
-                Span::styled(
-                    to_gigabytes(self.system.total_memory()).to_string(),
-                    Style::new().green().italic(),
-                ),
-            ]),
-            Line::from(vec![
-                Span::raw("Used Memory (GB): "),
-                Span::styled(
-                    to_gigabytes(self.system.used_memory()).to_string(),
-                    Style::new().green().italic(),
-                ),
-            ]),
-        ];
-        let info = Paragraph::new(text)
-            .block(Block::bordered().title("Paragraph"))
-            .style(Style::new().white().on_black())
-            .alignment(Alignment::Left)
-            .wrap(Wrap { trim: true });
-
-        let instructions = Title::from(Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]));
-        let block = Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .border_set(border::THICK);
-
-        let info_block = info.block(block);
-
-        info_block.render(area, buf)
-    }
 }
 
 fn main() -> io::Result<()> {
